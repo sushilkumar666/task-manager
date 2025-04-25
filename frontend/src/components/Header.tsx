ac44c11ee5540ca435b6a83a58559eab4844f61d
sushilkumar666 <sushilssharma064@gmail.com>
2025-04-25 12:16:14 +0530

    frontend-deploy-1

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useAuth } from '../context/UserProvider'
 import { User } from "lucide-react";
 
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import React, { ReactNode } from 'react'
 import { Outlet } from 'react-router-dom'
 import { useAuth } from '../context/UserProvider'
 import Header from './Header';
